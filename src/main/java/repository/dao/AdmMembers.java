b598106df03ea951ef74d8c400a4baeb5a6acad4
DamColson <Damien.colson@gmail.com>
2023-02-01 18:20:10 +0100

    First test

--- src/main/java/repository/dao/AdmMembers.java
+++ src/main/java/repository/dao/AdmMembers.java
@@ -1,2 +1,45 @@
+package repository.dao;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdmMembers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="member_id")
+    private long id;
+
+    @NotBlank
+    private String pseudo;
+
+    @NotBlank
+    @Email(message = "E-mail non valide")
+    @Column(unique = true)
+    private String mail;
+
+    @NotBlank
+    @Size(min = 8,message = "Votre mot de passe doit contenir au moins 8 caractère")
+    private String password;
+
+    /*
+    @ManyToOne
+    @JoinColumn(name = "rank_id")
+    private Rank rank;
+    */
+
+
+
 }
